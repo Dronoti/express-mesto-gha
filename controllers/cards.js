a8461faaf17c1d87d28e18fbedfdfd143b5e850b
Andrey Lazarev <dronoti@yandex.ru>
2022-07-29 16:58:43 +0300

    feat: expand cards requests

--- controllers/cards.js
+++ controllers/cards.js
@@ -37,3 +37,47 @@ module.exports.deleteCardById = (req, res) => {
       error: err.message,
     }));
 };
+
+module.exports.likeCard = (req, res) => {
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $addToSet: { likes: req.user._id } },
+    { new: true },
+  )
+    .then((card) => {
+      if (card) {
+        res.send(card);
+      } else {
+        res.status(404).send({
+          message: 'Карточка не найдена',
+          error: 'Not found',
+        });
+      }
+    })
+    .catch((err) => res.status(400).send({
+      message: 'Переданы некорректные данные',
+      error: err.message,
+    }));
+}
+
+module.exports.dislikeCard = (req, res) => {
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $pull: { likes: req.user._id } },
+    { new: true },
+  )
+    .then((card) => {
+      if (card) {
+        res.send(card);
+      } else {
+        res.status(404).send({
+          message: 'Карточка не найдена',
+          error: 'Not found',
+        });
+      }
+    })
+    .catch((err) => res.status(400).send({
+      message: 'Переданы некорректные данные',
+      error: err.message,
+    }));
+}
--- routes/cards.js
+++ routes/cards.js
@@ -1,10 +1,18 @@
 const express = require('express');
-const { createCard, getAllCards, deleteCardById } = require('../controllers/cards');
+const {
+  createCard,
+  getAllCards,
+  deleteCardById,
+  likeCard,
+  dislikeCard
+} = require('../controllers/cards');
 
 const router = express.Router();
 
 router.get('/', getAllCards);
 router.post('/', express.json(), createCard);
 router.delete('/:cardId', deleteCardById);
+router.put('/:cardId/likes', likeCard);
+router.delete('/:cardId/likes', dislikeCard);
 
 module.exports = router;

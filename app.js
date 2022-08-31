5b9d3cbb2f9e35d19a08554349a91769aa9e5423
Andrey Lazarev <dronoti@yandex.ru>
2022-08-31 22:50:20 +0300

    feat: add express-rate-limit

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
 const { errors } = require('celebrate');
+const rateLimit = require('express-rate-limit');
 const cors = require('cors');
 const usersRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
@@ -16,11 +17,18 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { PORT = 3000 } = process.env;
 const app = express();
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 500,
+});
+
 app.use(cors());
 
 app.use(requestLogger);
 
 app.use(helmet());
+
+app.use(limiter);
 
 mongoose.connect('mongodb://localhost:27017/mestodb', { useNewUrlParser: true });
 

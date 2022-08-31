46c97058afb2e37b5bec307e41a5919b56c6ff4a
Andrey Lazarev <dronoti@yandex.ru>
2022-08-31 22:02:16 +0300

    feat: add cors

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
 const { errors } = require('celebrate');
+const cors = require('cors');
 const usersRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
 const auth = require('./middlewares/auth');
@@ -14,11 +15,13 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { PORT = 3000 } = process.env;
 const app = express();
 
+app.use(cors());
+
 app.use(requestLogger);
 
-mongoose.connect('mongodb://localhost:27017/mestodb', { useNewUrlParser: true });
-
 app.use(helmet());
+
+mongoose.connect('mongodb://localhost:27017/mestodb', { useNewUrlParser: true });
 
 app.post('/signin', express.json(), loginDataIsValid, login);
 app.post('/signup', express.json(), registerDataIsValid, createUser);

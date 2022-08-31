4e695358a62a5e93429878e30439060d148b9269
Andrey Lazarev <dronoti@yandex.ru>
2022-08-31 21:47:19 +0300

    feat: add logger

--- app.js
+++ app.js
@@ -9,9 +9,12 @@ const NotFoundError = require('./errors/NotFoundError');
 const { createUser, login } = require('./controllers/users');
 const { loginDataIsValid, registerDataIsValid } = require('./middlewares/validator');
 const { handleErrors } = require('./middlewares/handleErrors');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 const app = express();
+
+app.use(requestLogger);
 
 mongoose.connect('mongodb://localhost:27017/mestodb', { useNewUrlParser: true });
 
@@ -23,6 +26,8 @@ app.post('/signup', express.json(), registerDataIsValid, createUser);
 app.use('/users', auth, usersRouter);
 app.use('/cards', auth, cardsRouter);
 app.use('*', auth, (req, res, next) => next(new NotFoundError('Страница не найдена')));
+
+app.use(errorLogger);
 
 app.use(errors());
 
